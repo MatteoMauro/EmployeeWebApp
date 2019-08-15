f3735ccd1ff094b08bdbeba081bc0a375b85f731
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-16 00:17:00 +0200

    implement getAllEmployees()

--- src/test/java/com/matteomauro/service/EmployeeServiceImpl.java
+++ src/test/java/com/matteomauro/service/EmployeeServiceImpl.java
@@ -2,15 +2,20 @@ package com.matteomauro.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.matteomauro.exception.EmployeeNotFoundException;
 import com.matteomauro.model.Employee;
+import com.matteomauro.repository.EmployeeRepository;
 
 public class EmployeeServiceImpl implements EmployeeService {
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
 	@Override
 	public List<Employee> getAllEmployees() {
-		// TODO Auto-generated method stub
-		return null;
+		return employeeRepository.findAll();
 	}
 
 	@Override
