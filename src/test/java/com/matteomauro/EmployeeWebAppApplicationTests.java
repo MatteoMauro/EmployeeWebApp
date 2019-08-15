909e6c4add319432314f4e0ff70b3ceb0ba531ab
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-15 22:03:08 +0200

    First push

---
+++ src/test/java/com/matteomauro/EmployeeWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matteomauro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
