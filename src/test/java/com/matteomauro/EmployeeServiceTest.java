42a0c72052418becc29dc802994551dc552690b2
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-16 00:25:19 +0200

    test getEmployeeById() when employee is found

--- src/test/java/com/matteomauro/EmployeeServiceTest.java
+++ src/test/java/com/matteomauro/EmployeeServiceTest.java
@@ -4,12 +4,15 @@ import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.matteomauro.exception.EmployeeNotFoundException;
 import com.matteomauro.model.Employee;
 import com.matteomauro.repository.EmployeeRepository;
 import com.matteomauro.service.EmployeeServiceImpl;
@@ -25,10 +28,17 @@ public class EmployeeServiceTest {
 
 	@Test
 	public void testGetAllEmployees() {
-		Employee employee1 = new Employee(null, "name1", "lastName1", 1000L, "role1");
-		Employee employee2 = new Employee(null, "name2", "lastName2", 2000L, "role2");
+		Employee employee1 = new Employee(1L, "name1", "lastName1", 1000L, "role1");
+		Employee employee2 = new Employee(2L, "name2", "lastName2", 2000L, "role2");
 		when(employeeRepository.findAll()).thenReturn(asList(employee1, employee2));
 		assertThat(employeeService.getAllEmployees()).containsExactly(employee1, employee2);
+	}
+	
+	@Test
+	public void testGetEmployeeByIdWhenIsPresent() throws Exception {
+		Employee employee = new Employee(1L, "name", "lastName", 1000L, "role");
+		when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee));
+		assertThat(employeeService.getEmployeeById(1L)).isEqualTo(employee);
 	}
 
 }
