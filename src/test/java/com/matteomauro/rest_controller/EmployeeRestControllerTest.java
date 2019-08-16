4c3d7818f803b432ee3da30c390c378b9ba088e0
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-17 01:12:46 +0200

    test find all users with existing employees

--- src/test/java/com/matteomauro/rest_controller/EmployeeRestControllerTest.java
+++ src/test/java/com/matteomauro/rest_controller/EmployeeRestControllerTest.java
@@ -1,6 +1,7 @@
 package com.matteomauro.rest_controller;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
@@ -14,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.matteomauro.model.Employee;
 import com.matteomauro.service.EmployeeService;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -41,7 +43,30 @@ public class EmployeeRestControllerTest {
 			get("/api/employees").
 		then().
 			statusCode(200).
+			assertThat().body(is(equalTo("[]")));
+	}
+
+	@Test
+	public void testFindAllUsersWithExistingEmployees() {
+		Employee employee1 = new Employee(1L, "name1", "lastName1", 1000L, "role1");
+		Employee employee2 = new Employee(2L, "name2", "lastName2", 2000L, "role2");
+		when(employeeService.getAllEmployees()).thenReturn(asList(employee1, employee2));
+
+		given().
+		when().
+			get("/api/employees").
+		then().
+			statusCode(200).
 			assertThat().
-				body(is(equalTo("[]")));
+			body("id[0]", equalTo(1), 
+				 "name[0]", equalTo("name1"), 
+				 "lastName[0]", equalTo("lastName1"), 
+				 "salary[0]", equalTo(1000),
+				 "role[0]", equalTo("role1"),
+				 "id[1]", equalTo(2), 
+				 "name[1]", equalTo("name2"), 
+				 "lastName[1]", equalTo("lastName2"), 
+				 "salary[1]", equalTo(2000),
+				 "role[1]", equalTo("role2"));
 	}
 }
