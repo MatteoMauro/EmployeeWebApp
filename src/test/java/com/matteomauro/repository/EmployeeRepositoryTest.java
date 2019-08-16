9a3b6335503f2ff37adc40d5276519b6199e81bc
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-17 00:02:28 +0200

    Created EmployeeRepositoryTest and added dependency for h2 database

---
+++ src/test/java/com/matteomauro/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.matteomauro.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EmployeeRepositoryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
