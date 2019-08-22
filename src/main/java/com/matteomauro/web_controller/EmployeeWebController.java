b3af3ee8ac35c570d11c6cf6cab9a3a41b2e48a3
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-23 00:06:54 +0200

    implement /save end-point

--- src/main/java/com/matteomauro/web_controller/EmployeeWebController.java
+++ src/main/java/com/matteomauro/web_controller/EmployeeWebController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.matteomauro.exception.EmployeeNotFoundException;
 import com.matteomauro.model.Employee;
@@ -40,6 +41,17 @@ public class EmployeeWebController {
 	public String newEmployee(Model model) {
 		model.addAttribute("employee", new Employee());
 		return "edit";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(Employee employee) throws EmployeeNotFoundException {
+		final Long id = employee.getId();
+		if (id == null) {
+			employeeService.insertNewEmployee(employee);
+		} else {
+			employeeService.updateEmployeeById(id, employee);
+		}
+		return "redirect:/";
 	}
 
 }
