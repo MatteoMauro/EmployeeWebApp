47ebd1298599a3ced6a6163d81be748622a817eb
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-22 23:10:23 +0200

    implement get list of employees and put it inside model

--- src/main/java/com/matteomauro/web_controller/EmployeeWebController.java
+++ src/main/java/com/matteomauro/web_controller/EmployeeWebController.java
@@ -1,13 +1,21 @@
 package com.matteomauro.web_controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.matteomauro.service.EmployeeService;
 
 @Controller
 public class EmployeeWebController {
 
+	@Autowired
+	private EmployeeService employeeService;
+
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("employees", employeeService.getAllEmployees());
 		return "index";
 	}
 }
