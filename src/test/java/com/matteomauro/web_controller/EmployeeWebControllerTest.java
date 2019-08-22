1299054462c5c200e898f584ff0c96bc434e0c20
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-22 23:07:44 +0200

    test get all employees in index

--- src/test/java/com/matteomauro/web_controller/EmployeeWebControllerTest.java
+++ src/test/java/com/matteomauro/web_controller/EmployeeWebControllerTest.java
@@ -1,15 +1,24 @@
 package com.matteomauro.web_controller;
 
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.matteomauro.model.Employee;
+import com.matteomauro.service.EmployeeService;
 import com.matteomauro.web_controller.EmployeeWebController;
 
 @RunWith(SpringRunner.class)
@@ -18,9 +27,32 @@ public class EmployeeWebControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+	
+	@MockBean
+	private EmployeeService employeeService;
 
 	@Test
 	public void testStatus200() throws Exception {
 		mvc.perform(get("/")).andExpect(status().is2xxSuccessful());
 	}
+
+	@Test
+	public void test_HomeView_ShowsEmployees() throws Exception {
+		List<Employee> employees = asList(new Employee(1L, "name", "lastName", 1000L, "role"));
+		when(employeeService.getAllEmployees()).thenReturn(employees);
+		
+		mvc.perform(get("/")).
+			andExpect(view().name("index")).
+			andExpect(model().attribute("employees", employees));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
