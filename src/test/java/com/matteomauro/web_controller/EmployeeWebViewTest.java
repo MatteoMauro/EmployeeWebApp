010c9d1b2aaa57fcb41230a00e5012fded3023ce
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-23 21:21:42 +0200

    test table of employees in index

--- src/test/java/com/matteomauro/web_controller/EmployeeWebViewTest.java
+++ src/test/java/com/matteomauro/web_controller/EmployeeWebViewTest.java
@@ -2,7 +2,7 @@ package com.matteomauro.web_controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 
 import org.junit.Test;
@@ -14,6 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.matteomauro.model.Employee;
 import com.matteomauro.service.EmployeeService;
 
 @RunWith(SpringRunner.class)
@@ -37,5 +39,21 @@ public class EmployeeWebViewTest {
 		when(employeeService.getAllEmployees()).thenReturn(emptyList());
 		HtmlPage page = this.webClient.getPage("/");
 		assertThat(page.getBody().getTextContent()).contains("No Employee");
+	}
+
+	@Test
+	public void testHomePageWithEmployees_shouldShowThemInATable() throws Exception {
+		Employee employee1 = new Employee(1L, "name1", "lastName1", 1000L, "role1");
+		Employee employee2 = new Employee(2L, "name2", "lastName2", 2000L, "role2");
+		when(employeeService.getAllEmployees()).thenReturn(asList(employee1, employee2));
+		
+		HtmlPage page = this.webClient.getPage("/");
+		assertThat(page.getBody().getTextContent()).doesNotContain("No Employee");
+		HtmlTable table = page.getHtmlElementById("employee_table");
+		assertThat(table.asText()).isEqualTo(
+				"ID	Name	LastName	Salary	Role\n" +
+				"1	name1	lastName1	1000	role1\n" +
+				"2	name2	lastName2	2000	role2"
+		);
 	}
 }
